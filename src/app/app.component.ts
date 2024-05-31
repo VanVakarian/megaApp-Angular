4a52dec76e28671dccaed2afbb82fe8754313a80
VanVakarian <void@vslav.dev>
2024-05-31 23:18:29 +0500

    🏗 [036] improved navbar design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DateAdapter, MatNativeDateModule } from '@angular/material/core';
-// import { MatSidenavContent } from '@angular/material/sidenav';
+import { Subscription, debounceTime, fromEvent, merge, throttleTime } from 'rxjs';
 
 import { NavbarMobileComponent } from './components/navbars/navbar-mobile/navbar-mobile.component';
 import { NavbarDesktopComponent } from './components/navbars/navbar-desktop/navbar-desktop.component';
@@ -11,40 +11,41 @@ import { NavbarDesktopComponent } from './components/navbars/navbar-desktop/navb
   standalone: true,
   imports: [NavbarDesktopComponent, NavbarMobileComponent, RouterOutlet, MatNativeDateModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrl: './app.component.scss',
 })
-export class MainAppComponent implements OnInit {
-  // @ViewChild('scrollable') scrollable!: MatSidenavContent;
+export class MainAppComponent implements OnInit, OnDestroy {
+  title = 'megaApp';
+  private scrollSubscription: Subscription | undefined;
+  @ViewChild('header') header!: ElementRef;
 
-  title = 'megaapp';
-
-  menuOpened = false;
-
-  // public foodService: FoodService, // public moneyService: MoneyService,
-  constructor(private dateAdapter: DateAdapter<Date>) {
-    // this.foodService.diaryEntryClickedScroll$
-    //   .pipe(
-    //     delay(190) // Waiting for expansion panel animation to finish before scrolling. Otherwise it scrolls to the wrong place.
-    //   )
-    //   .subscribe((clickedElem: ElementRef) => {
-    //     const scrollPx = clickedElem.nativeElement.getBoundingClientRect().top - 50;
-    //     this.scrollable.getElementRef().nativeElement.scrollBy({ top: scrollPx, behavior: 'smooth' });
-    //   });
-  }
-
-  hamburgerPressed(hamburgerCheckboxStatus: boolean) {
-    this.menuOpened = hamburgerCheckboxStatus;
-  }
-
-  closeMenu() {
-    this.menuOpened = false;
-  }
+  constructor(private dateAdapter: DateAdapter<Date>) {}
 
   ngOnInit(): void {
-    // making monday to be the first day of the week in a calendar
     this.dateAdapter.setLocale('ru-RU');
-    this.dateAdapter.getFirstDayOfWeek = () => {
-      return 1;
-    };
+    this.dateAdapter.getFirstDayOfWeek = () => 1;
+
+    // Totally overkill method to detect when to put shadow on fixed navbar, but that shadow looks too nice...
+    const scroll$ = fromEvent(window, 'scroll');
+    const throttledScroll$ = scroll$.pipe(throttleTime(200));
+    const debouncedScroll$ = scroll$.pipe(debounceTime(10));
+    this.scrollSubscription = merge(throttledScroll$, debouncedScroll$).subscribe(() => this.onScroll());
+  }
+
+  ngOnDestroy(): void {
+    if (this.scrollSubscription) {
+      this.scrollSubscription.unsubscribe();
+    }
+  }
+
+  onScroll(): void {
+    if (!this.header) {
+      return;
+    }
+    const headerEl = this.header.nativeElement;
+    if (window.scrollY > 0) {
+      headerEl.classList.add('shadow-md');
+    } else {
+      headerEl.classList.remove('shadow-md');
+    }
   }
 }
