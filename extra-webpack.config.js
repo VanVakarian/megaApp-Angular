35854c08b683ab33b7c0fe42a3bd17e7ed05204d
VanVakarian <void@vslav.dev>
2025-01-25 00:41:10 +0300

    🔧 console.log with commit and build date

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,4 +1,25 @@
 const path = require('path');
+const webpack = require('webpack');
+const childProcess = require('child_process');
+const fs = require('fs');
+
+const getGitInfo = () => {
+  try {
+    return childProcess.execSync('git rev-parse --short HEAD').toString().trim();
+  } catch (e) {
+    return 'unknown';
+  }
+};
+
+class BuildInfoPlugin {
+  // making a file in assets with frontend app build info
+  apply(compiler) {
+    compiler.hooks.done.tap('BuildInfoPlugin', () => {
+      const buildInfo = `console.log("Commit: [${getGitInfo()}] built at [${new Date().toLocaleTimeString()}]");`;
+      fs.writeFileSync('./src/assets/build-info.js', buildInfo);
+    });
+  }
+}
 
 module.exports = {
   resolve: {
@@ -6,4 +27,5 @@ module.exports = {
       '@app': path.resolve(__dirname, '@app/'),
     },
   },
+  plugins: [new BuildInfoPlugin()],
 };
